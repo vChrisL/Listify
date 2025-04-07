a38d365bbb0061ad20a56d60b77af29ca2523f37
Chris <4knowin9@gmail.com>
2025-04-07 17:29:35 -0300

    Added initial Header component layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import {BookIcon, MenuIcon} from "../util/Icons.tsx";
+
+/**
+ * Mobile header component.
+ */
+export function Header() {
+  return (
+    <header className={"flex flex-row"}>
+      <button>
+        <MenuIcon style={"w-10 h-10 stroke-black"}/>
+      </button>
+
+      <span className={"w-1/5"}/>
+
+      <div className={"flex flex-row justify-center items-center gap-2"}>
+        <BookIcon style={"w-10 h-10 stroke-black"}/>
+        <h1>Listify</h1>
+      </div>
+    </header>
+  )
+}
