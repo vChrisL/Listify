f95263d02765d61d3eba22518c73cd749a37617f
Chris <4knowin9@gmail.com>
2025-04-10 17:29:04 -0300

    Implemented NewList Button

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import {Header} from "../components/Header.tsx";
 import { Sidebar } from "../components/Sidebar.tsx";
 import { useSidebarStore } from "../stores/MenuStore.tsx";
-import {DashboardIcon} from "../util/Icons.tsx";
+import {AddIcon, DashboardIcon} from "../util/Icons.tsx";
 
 /**
  * Dashboard page.
@@ -21,13 +21,16 @@ export function DashboardPage() {
         </div>
 
         <div className={"grid grid-cols-2 gap-4 overflow-y-auto"}>
-
+          <button className={"flex flex-col gap-2 items-center justify-center py-12 px-2 bg-tertiary-bg rounded-lg"}>
+            <AddIcon style={"w-12 h-12 stroke-text-color"}/>
+            <h3>New List</h3>
+          </button>
           {
             Array.from({length: 12}, () =>
-              <div className={"flex flex-col gap-2 items-center justify-center py-12 px-2 bg-tertiary-bg rounded-lg"}>
+              <button className={"flex flex-col gap-2 items-center justify-center py-12 px-2 bg-tertiary-bg rounded-lg"}>
                 <DashboardIcon style={"w-12 h-12 stroke-text-color"}/>
                 <h3>List Name Goes Here</h3>
-              </div>
+              </button>
             )
           }
 
