36659fb7ac89d24e2115aab06697d2b974aca39b
Chris <4knowin9@gmail.com>
2025-04-16 15:58:40 -0300

    Added initial React-Router implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { DashboardPage } from './pages/DashboardPage.tsx';
+import { ErrorPage } from './pages/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {path: "/", element: <App/>},
+  {path: "/dashboard", element: <DashboardPage/>},
+  {path: "*", element: <ErrorPage/>},
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
