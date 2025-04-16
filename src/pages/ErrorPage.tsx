75266f660aff18baff50dc5b084511d7dad6a099
Chris <4knowin9@gmail.com>
2025-04-16 15:59:13 -0300

    Implemented ErrorPage

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export function ErrorPage() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-8 h-full w-full">
+      <h1>404 Not Found</h1>
+      <h1>This page doesn't exist!</h1>
+      <Link to={"/"}>
+        <button className="bg-secondary-accent rounded-lg p-2">Take Me Back!</button>
+      </Link>
+    </div>
+  )
+}
