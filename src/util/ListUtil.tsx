baa5a9a7fd88c7fc8637ce09d7d4ae434ee97ce2
Chris <4knowin9@gmail.com>
2025-04-19 10:16:16 -0300

    Added ListIcons module

---
+++ src/util/ListUtil.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { BookIcon, ClipboardIcon, ControllerIcon, GameIcon, InternetIcon, LightbulbIcon, MusicIcon, UtensilIcon, WrenchIcon } from "./Icons";
+
+export const ListIcons: ReactNode[] = [
+  <MusicIcon      style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+  <WrenchIcon     style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+  <GameIcon       style="w-8 h-8 fill-text-color lg:w-12 lg:h-12"/>,
+  <ControllerIcon style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+  <UtensilIcon    style="w-8 h-8 fill-text-color lg:w-12 lg:h-12"/>,
+  <LightbulbIcon  style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+  <BookIcon       style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+  <InternetIcon   style="w-8 h-8 fill-text-color lg:w-12 lg:h-12"/>,
+  <ClipboardIcon  style="w-8 h-8 stroke-text-color lg:w-12 lg:h-12"/>,
+];
