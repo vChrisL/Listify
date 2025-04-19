a445b29d489a0a32d40941b577eded0f974ebdd7
Chris <4knowin9@gmail.com>
2025-04-19 10:15:58 -0300

    Added export to ColorMap

--- src/util/ColorUtil.tsx
+++ src/util/ColorUtil.tsx
@@ -1,4 +1,4 @@
-const ColorMap: Map<string, string> = new Map([
+export const ColorMap: Map<string, string> = new Map([
   ["blue",    "#688CDA"],
   ["teal",    "#6BD4DB"],
   ["green",   "#72C993"],
