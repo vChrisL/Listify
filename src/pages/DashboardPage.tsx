06c1b6f8944ed08b691367948b94128af8474903
Chris <4knowin9@gmail.com>
2025-04-20 10:09:38 -0300

    Adjusted list card size & container justification

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -37,9 +37,9 @@ export function DashboardPage() {
         </div>
         <hr className="hidden lg:block bg-accent w-full border-none h-0.5"/>
 
-        <div className={"flex flex-row flex-wrap gap-4 overflow-y-auto w-full xl:grid-cols-7 md:grid-cols-5 sm:grid-cols-3"}>
+        <div className={"flex flex-row flex-wrap justify-center gap-4 overflow-y-auto w-full md:justify-start"}>
           <Link to={"/newList"}>
-            <button className={"flex flex-col gap-2 items-center justify-center py-12 px-2 bg-secondary-accent rounded-lg w-36 h-52 lg:w-40"}>
+            <button className={"flex flex-col gap-2 items-center justify-center py-12 px-2 bg-secondary-accent rounded-lg w-34 h-52 lg:w-40"}>
               <AddIcon style={"w-12 h-12 stroke-text-color"}/>
               <h3>New List</h3>
             </button>
