403301acc52b03bffbbd2e67ffd1c63589ac32a8
Chris <4knowin9@gmail.com>
2025-04-24 09:40:35 -0300

    Removed react router route to Dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import {DashboardPage} from "./pages/DashboardPage.tsx";
 
 function App() {
   return (
     <>
-
+      <DashboardPage></DashboardPage>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { DashboardPage } from './pages/DashboardPage.tsx';
 import { ErrorPage } from './pages/ErrorPage.tsx';
 import { NewListPage } from './pages/NewListPage.tsx';
 
 const router = createBrowserRouter([
   {path: "/", element: <App/>},
-  {path: "/dashboard", element: <DashboardPage/>},
   {path: "/new-list", element: <NewListPage/>},
   {path: "*", element: <ErrorPage/>},
 ]);
