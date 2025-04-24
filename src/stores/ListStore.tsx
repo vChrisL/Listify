7b93c9f909db11bcfbe64d3535f8c4f726550026
Chris <4knowin9@gmail.com>
2025-04-24 13:49:06 -0300

    Implemented localStorage

--- src/stores/ListStore.tsx
+++ src/stores/ListStore.tsx
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import { ListObject } from "../types/ListType";
 
+const LOCAL_STORAGE_NAME = "listifyLists";
+
 type ListStore = {
   lists: ListObject[],
   addList: (newList: ListObject) => void,
@@ -9,10 +11,11 @@ type ListStore = {
 }
 
 export const useListStore = create<ListStore>(set => ({
-  lists: [],
+  lists: JSON.parse(localStorage.getItem(LOCAL_STORAGE_NAME) ?? "[]"),
   addList: (newList: ListObject) => {
     set((state) => {
       const newListState = [...state.lists, newList];
+      localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(newListState));
       return {lists: newListState}
     })
   }
