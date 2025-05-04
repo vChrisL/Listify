e1a6b939fac4c44758fec1ed3cfa96ccd1b78ada
Chris <4knowin9@gmail.com>
2025-05-04 10:38:52 -0300

    Added Import / Export Buttons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useSidebarStore } from "../stores/MenuStore";
-import { BookIcon, DashboardIcon } from "../util/Icons";
+import { BookIcon, DashboardIcon, ExportIcon, ImportIcon } from "../util/Icons";
 import { useOnClickOutside } from "../util/OnClickOutsideHook";
 import { ListShortcut } from "./List";
 import { tryGetColor } from "../util/ColorUtil.tsx";
@@ -23,7 +23,7 @@ export function Sidebar() {
   return (
     <motion.aside 
       ref={sidebarRef} 
-      className="absolute flex flex-col items-center gap-4 top-0 left-0 bg-secondary-bg w-64 h-screen rounded-r-lg p-4"
+      className="absolute flex flex-col items-center justify-between gap-4 top-0 left-0 bg-secondary-bg w-64 h-screen rounded-r-lg p-4"
       initial={{x: "-100vw"}}
       animate={{
         x: 0,
@@ -33,27 +33,39 @@ export function Sidebar() {
       }}
       exit={{x: "-100vw"}}
     >
-      <div className={"flex flex-row justify-center items-center gap-2"}>
-        <BookIcon style={"w-10 h-10 stroke-text-color"}/>
-        <h1>Listify</h1>
+      <div className="flex flex-col items-center gap-4 w-full">
+        <div className={"flex flex-row justify-center items-center gap-2"}>
+          <BookIcon style={"w-10 h-10 stroke-text-color"}/>
+          <h1>Listify</h1>
+        </div>
+
+        <div className="w-full">
+          <Link to={"/"}>
+            <button className="flex flex-row gap-2 items-center justify-center w-full bg-secondary-accent rounded-lg p-2 mb-8" onClick={() => {setDisplaySidebar(false)}}>
+              <DashboardIcon style="w-8 h-8 stroke-text-color"/>
+              <h3>Dashboard</h3>
+            </button>
+          </Link>
+          
+          <div className="flex flex-col gap-2">
+            {
+              lists.map((list) => 
+                <ListShortcut key={list.id} listObj={list}/>
+              )
+            }
+          </div>
+        </div>
       </div>
 
-      <div className="w-full">
-        <Link to={"/"}>
-          <button className="flex flex-row gap-2 items-center justify-center w-full bg-secondary-accent rounded-lg p-2 mb-8" onClick={() => {setDisplaySidebar(false)}}>
-            <DashboardIcon style="w-8 h-8 stroke-text-color"/>
-            <h3>Dashboard</h3>
-          </button>
-        </Link>
-        
-        <div className="flex flex-col gap-2">
-          {
-            lists.map((list) => 
-              <ListShortcut key={list.id} listObj={list}/>
-            )
-          }
-        </div>
-        
+      <div className="flex flex-row justify-between gap-8">
+        <button className="flex flex-row gap-2">
+          <ExportIcon style="w-6 h-6 fill-text-color"/>
+          <h3>Export</h3>
+        </button>
+        <button className="flex flex-row gap-2">
+          <ImportIcon style="w-6 h-6 fill-text-color"/>
+          <h3>Import</h3>
+        </button>
       </div>
     </motion.aside>
   )
@@ -66,27 +78,40 @@ export function DesktopSidebar() {
   const lists = useListStore(state => state.lists);
 
   return (
-    <aside className="hidden lg:flex flex-col items-center gap-4 w-1/6 p-4 pr-0">
-      <div className={"flex flex-row justify-center items-center gap-2"}>
-        <BookIcon style={"w-10 h-10 stroke-text-color"}/>
-        <h1>Listify</h1>
+    <aside className="hidden lg:flex flex-col items-center justify-between gap-4 w-1/6 p-4 pr-0">
+      <div className="flex flex-col items-center gap-4 w-full">
+        <div className={"flex flex-row justify-center items-center gap-2"}>
+          <BookIcon style={"w-10 h-10 stroke-text-color"}/>
+          <h1>Listify</h1>
+        </div>
+
+        <div className="w-full">
+          <Link to={"/"}>
+            <button className="flex flex-row gap-2 items-center justify-center w-full bg-secondary-accent rounded-lg p-2 mb-8">
+              <DashboardIcon style="w-8 h-8 stroke-text-color"/>
+              <h3>Dashboard</h3>
+            </button>
+          </Link>
+          
+          <div className="flex flex-col gap-2">
+            {
+              lists.map((list) => 
+                <ListShortcut key={list.id} listObj={list}/>
+              )
+            }
+          </div>
+        </div>
       </div>
 
-      <div className="w-full">
-        <Link to={"/"}>
-          <button className="flex flex-row gap-2 items-center justify-center w-full bg-secondary-accent rounded-lg p-2 mb-8">
-            <DashboardIcon style="w-8 h-8 stroke-text-color"/>
-            <h3>Dashboard</h3>
-          </button>
-        </Link>
-        
-        <div className="flex flex-col gap-2">
-          {
-            lists.map((list) => 
-              <ListShortcut key={list.id} listObj={list}/>
-            )
-          }
-        </div>
+      <div className="flex flex-row justify-between gap-8">
+        <button className="flex flex-row gap-2">
+          <ExportIcon style="w-6 h-6 fill-text-color"/>
+          <h3>Export</h3>
+        </button>
+        <button className="flex flex-row gap-2">
+          <ImportIcon style="w-6 h-6 fill-text-color"/>
+          <h3>Import</h3>
+        </button>
       </div>
     </aside>
   )
--- src/util/Icons.tsx
+++ src/util/Icons.tsx
@@ -215,6 +215,30 @@ export function CheckmarkIcon({style}: IconProps) {
   )
 }
 
+/**
+ * Export icon.
+ * @param style Tailwind style classes to be applied. Uses fill for color.
+ */
+export function ExportIcon({style}: IconProps) {
+  return (
+    <svg className={style} xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+      <path fill="inherit" d="M8.71 7.71L11 5.41V15a1 1 0 0 0 2 0V5.41l2.29 2.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42l-4-4a1 1 0 0 0-.33-.21a1 1 0 0 0-.76 0a1 1 0 0 0-.33.21l-4 4a1 1 0 1 0 1.42 1.42M21 14a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-4a1 1 0 0 0-2 0v4a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-4a1 1 0 0 0-1-1"></path>
+    </svg>
+  )
+}
+
+/**
+ * Import icon.
+ * @param style Tailwind style classes to be applied. Uses fill for color.
+ */
+export function ImportIcon({style}: IconProps) {
+  return (
+    <svg className={style} xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+      <path fill="inherit" d="M21 14a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-4a1 1 0 0 0-2 0v4a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-4a1 1 0 0 0-1-1m-9.71 1.71a1 1 0 0 0 .33.21a.94.94 0 0 0 .76 0a1 1 0 0 0 .33-.21l4-4a1 1 0 0 0-1.42-1.42L13 12.59V3a1 1 0 0 0-2 0v9.59l-2.29-2.3a1 1 0 1 0-1.42 1.42Z"></path>
+    </svg>
+  )
+}
+
 
 
 //////////////////////////// LIST ICONS BELOW ////////////////////////////
