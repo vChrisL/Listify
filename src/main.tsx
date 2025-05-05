ed5799dbc0550d05a3af43714d358408c4463d3c
Chris <4knowin9@gmail.com>
2025-05-05 09:30:44 -0300

    Module Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,34 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ErrorPage } from './pages/ErrorPage.tsx';
-import { NewListPage } from './pages/NewListPage.tsx';
-
-const router = createBrowserRouter([
-  {path: "/", element: <App/>},
-  {path: "/new-list", element: <NewListPage/>},
-  {
-    path: "/list", 
-    element: <div>List page</div>,
-    children: [
-      {
-        path: "delete",
-        element: <div>Delete list page</div>
-      },
-      {
-        path: "edit",
-        element: <div>Edit list page</div>
-      }
-    ]
-  },
-
-  {path: "*", element: <ErrorPage/>},
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <RouterProvider router={router}/> */}
     <App></App>
   </StrictMode>,
 )
