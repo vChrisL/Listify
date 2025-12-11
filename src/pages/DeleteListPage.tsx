fe1634278ce063f1f58754529eff3a598f81e72f
Chris <4knowin9@gmail.com>
2025-12-11 11:55:14 -0400

    Fix: Added useEffect() To Handle OnMount Logic

--- src/pages/DeleteListPage.tsx
+++ src/pages/DeleteListPage.tsx
@@ -2,24 +2,34 @@ import { AnimatePresence } from "motion/react";
 import { Header } from "../components/Header.tsx";
 import { DesktopSidebar, Sidebar } from "../components/Sidebar.tsx";
 import { useSidebarStore } from "../stores/MenuStore.tsx";
-import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useListStore } from "../stores/ListStore.tsx";
+import { useEffect, useState } from "react";
+import { ListObject } from "../types/ListType.tsx";
 
 /**
  * Delete List Page.
  */
 export function DeleteListPage() {
+  const navigate = useNavigate();
   const isDisplaySidebar = useSidebarStore(state => state.isDisplayed);
   
   const {listid} = useParams();
-  if (listid === undefined) return <Navigate replace to="*"/>;
   
-  const list = useListStore(state => state.lists).find(obj => obj.id === listid)
-  if(list === undefined) return <Navigate replace to="/*"/>;
+  const listList = useListStore(state => state.lists)
+  const [list, setList] = useState<ListObject>();
   
-  const navigate = useNavigate();
   const deleteList = useListStore(state => state.deleteList);
   
+  useEffect(() => {
+    const list = listList.find(obj => obj.id === listid);
+    
+    if (list === undefined || listid === undefined)
+      navigate("/");
+    
+    setList(list);
+  }, []);
+
   /**
    * Handles delete list form submission.
    * @param e Form event
@@ -45,10 +55,10 @@ export function DeleteListPage() {
         className={"flex flex-col gap-4 items-center p-4 rounded-t-lg bg-secondary-bg h-[calc(100%-4rem)] lg:h-full lg:grow lg:mt-4 lg:mr-4 lg:items-start lg:p-8 overflow-y-auto"}
         onSubmit={(e) => { handleSubmit(e) }}
       >
-        <h1 className="text-center mx-auto mt-24">Are you sure you wish to delete the list <span className="text-red-400">{list.name}</span>?</h1>
+        <h1 className="text-center mx-auto mt-24">Are you sure you wish to delete the list <span className="text-red-400">{list?.name}</span>?</h1>
 
         <div className="flex flex-row gap-4 font-semibold mx-auto">
-          <Link replace to={`/list/${list.id}/edit`}>
+          <Link replace to={`/list/${list?.id}/edit`}>
             <button type="button">Cancel</button>
           </Link>
           <button type="submit">Confirm</button>
